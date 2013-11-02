3861469284bef9faf2a1f20a45e69dfb60f848ac
SonicGD <sonicgd@gmail.com>
2013-11-02 23:29:13 +0600

    remove some duplicates

---
+++ Google/Api/Ads/AdWords/Util/ReportUtils.require.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("../v201306/classes/Selector.php");
+require_once("../v201306/classes/Predicate.php");
+require_once("../v201306/classes/PredicateOperator.php");
+require_once("../v201306/classes/DateRange.php");
+require_once("../v201306/classes/OrderBy.php");
+require_once("../v201306/classes/Paging.php");
+require_once("../v201306/classes/SortOrder.php");
+require_once("../v201306/classes/ReportDefinitionReportType.php");

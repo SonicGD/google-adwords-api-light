2e799eb32e01f9bcb8158b0dbe2db61c0e7867e8
SonicGD <sonicgd@gmail.com>
2013-11-02 23:31:32 +0600

    fix relative paths

--- Google/Api/Ads/AdWords/Util/ReportUtils.require.php
+++ Google/Api/Ads/AdWords/Util/ReportUtils.require.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once("../v201306/classes/Selector.php");
-require_once("../v201306/classes/Predicate.php");
-require_once("../v201306/classes/PredicateOperator.php");
-require_once("../v201306/classes/DateRange.php");
-require_once("../v201306/classes/OrderBy.php");
-require_once("../v201306/classes/Paging.php");
-require_once("../v201306/classes/SortOrder.php");
-require_once("../v201306/classes/ReportDefinitionReportType.php");
+require_once(dirname(__FILE__) . '/../v201306/classes/Selector.php');
+require_once(dirname(__FILE__) . '/../v201306/classes/Predicate.php');
+require_once(dirname(__FILE__) . '/../v201306/classes/PredicateOperator.php');
+require_once(dirname(__FILE__) . '/../v201306/classes/DateRange.php');
+require_once(dirname(__FILE__) . '/../v201306/classes/OrderBy.php');
+require_once(dirname(__FILE__) . '/../v201306/classes/Paging.php');
+require_once(dirname(__FILE__) . '/../v201306/classes/SortOrder.php');
+require_once(dirname(__FILE__) . '/../v201306/classes/ReportDefinitionReportType.php');

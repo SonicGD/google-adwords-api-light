8b790dd0676e60a89a08c71ffaf2c386ff069901
SonicGD <sonicgd@gmail.com>
2014-12-16 11:41:38 +0500

    fix require

--- Google/Api/Ads/AdWords/Util/ReportUtils.require.php
+++ Google/Api/Ads/AdWords/Util/ReportUtils.require.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once(dirname(__FILE__) . '/../v201406/classes/Selector.php');
-require_once(dirname(__FILE__) . '/../v201406/classes/Predicate.php');
-require_once(dirname(__FILE__) . '/../v201406/classes/PredicateOperator.php');
-require_once(dirname(__FILE__) . '/../v201406/classes/DateRange.php');
-require_once(dirname(__FILE__) . '/../v201406/classes/OrderBy.php');
-require_once(dirname(__FILE__) . '/../v201406/classes/Paging.php');
-require_once(dirname(__FILE__) . '/../v201406/classes/SortOrder.php');
-require_once(dirname(__FILE__) . '/../v201406/classes/ReportDefinitionReportType.php');
+require_once(dirname(__FILE__) . '/../v201409/classes/Selector.php');
+require_once(dirname(__FILE__) . '/../v201409/classes/Predicate.php');
+require_once(dirname(__FILE__) . '/../v201409/classes/PredicateOperator.php');
+require_once(dirname(__FILE__) . '/../v201409/classes/DateRange.php');
+require_once(dirname(__FILE__) . '/../v201409/classes/OrderBy.php');
+require_once(dirname(__FILE__) . '/../v201409/classes/Paging.php');
+require_once(dirname(__FILE__) . '/../v201409/classes/SortOrder.php');
+require_once(dirname(__FILE__) . '/../v201409/classes/ReportDefinitionReportType.php');

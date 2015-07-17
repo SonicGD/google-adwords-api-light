6ece428322e5e7d05e137f1e78ead6b58420d6ae
SonicGD <sonicgd@gmail.com>
2015-07-17 11:27:35 +0500

    upd util

--- Google/Api/Ads/AdWords/Util/ReportUtils.require.php
+++ Google/Api/Ads/AdWords/Util/ReportUtils.require.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once dirname(__FILE__).'/../v201502/classes/Selector.php';
-require_once dirname(__FILE__).'/../v201502/classes/Predicate.php';
-require_once dirname(__FILE__).'/../v201502/classes/PredicateOperator.php';
-require_once dirname(__FILE__).'/../v201502/classes/DateRange.php';
-require_once dirname(__FILE__).'/../v201502/classes/OrderBy.php';
-require_once dirname(__FILE__).'/../v201502/classes/Paging.php';
-require_once dirname(__FILE__).'/../v201502/classes/SortOrder.php';
-require_once dirname(__FILE__).'/../v201502/classes/ReportDefinitionReportType.php';
+require_once dirname(__FILE__).'/../v201506/classes/Selector.php';
+require_once dirname(__FILE__).'/../v201506/classes/Predicate.php';
+require_once dirname(__FILE__).'/../v201506/classes/PredicateOperator.php';
+require_once dirname(__FILE__).'/../v201506/classes/DateRange.php';
+require_once dirname(__FILE__).'/../v201506/classes/OrderBy.php';
+require_once dirname(__FILE__).'/../v201506/classes/Paging.php';
+require_once dirname(__FILE__).'/../v201506/classes/SortOrder.php';
+require_once dirname(__FILE__).'/../v201506/classes/ReportDefinitionReportType.php';
